5c6a96e6b678b84bdc475939646d07b9dd594b7d
Mateus <joao.santos@fabricainfo.com>
2025-02-21 20:38:19 -0300

    [feat] Front
    Paginação ok falta ajustar scroll

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
-import Admin from './pages/Admin'; 
+import Admin from './pages/Admin';
+import { Box } from '@mui/material';
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/admin" element={<Admin />} />
-            </Routes>
-        </Router>
+    <Box sx={{ width: "100vw", height: "100vh", overflowX: "hidden" }}>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/admin" element={<Admin />} />
+                </Routes>
+            </Router>
+        </Box>
     );
 };
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8166/api', 
+    baseURL: 'http://localhost:8167/api', 
 });
 
 export default api;
