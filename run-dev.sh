3924dec05da65f2b805478736da401be75eb2510
Mateus <joao.santos@fabricainfo.com>
2025-02-23 16:26:08 -0300

    Atualizando o top musicas

---
+++ run-dev.sh
@@ -0,0 +1,12 @@
+cp .env.example .env
+if [ $? -ne 0 ]; then
+    echo "Erro ao copiar o arquivo .env.example para .env. Abortando."
+    exit 1
+fi
+echo "Subindo os containers do Docker..."
+docker-compose up -d
+if [ $? -ne 0 ]; then
+    echo "Erro ao subir os containers do Docker. Abortando."
+    exit 1
+fi
+
